af5a0734e172e5dfc72c9ff1c046f0b2d1c8a713
zeuros <gery.zablocki@gmail.com>
2024-05-20 10:55:53 +0200

    better commit log

--- src/app/models/git-repository.ts
+++ src/app/models/git-repository.ts
@@ -1,4 +1,5 @@
 import {ReadCommitResult} from "isomorphic-git";
+import {BranchesAndLogs} from "../services/electron-ipc-api.service";
 
 export class GitRepository {
     constructor(
@@ -6,9 +7,8 @@ export class GitRepository {
         public name: string,
         public sizes = [20, 50, 30], // panels sizes
         public selected = true, // currently selected
-        public localBranches: string[] = [],
+        public branchesAndLogs: BranchesAndLogs = {},
         public remoteBranches: string[] = [],
-        public commits: ReadCommitResult[] = [],
         public remotes: { remote: string; url: string }[] = [],
     ) {
     }
