0a41029fc2102c14c20d56d5052ee415f888f1c9
zeuros <13236310+zeuros@users.noreply.github.com>
2025-02-11 14:05:44 +0100

    Reboot project using https://github.com/maximegris/angular-electron/tree/main

---
+++ src/app/components/dialogs/clone-or-open-directory-dialog/clone-or-open-directory-dialog.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {ElectronIpcApiService} from "../../../services/electron-ipc-api.service";
+import {PopupService} from "../../../services/popup.service";
+import {FieldsetModule} from "primeng/fieldset";
+import {ButtonModule} from "primeng/button";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {PanelModule} from "primeng/panel";
+import {CardModule} from "primeng/card";
+
+@Component({
+    selector: 'gitgud-clone-or-open-directory-dialog',
+    standalone: true,
+    imports: [
+        FieldsetModule,
+        ButtonModule,
+        FloatLabelModule,
+        InputTextModule,
+        FormsModule,
+        PanelModule,
+        CardModule
+    ],
+    templateUrl: './clone-or-open-directory-dialog.component.html',
+    styleUrl: './clone-or-open-directory-dialog.component.scss'
+})
+export class CloneOrOpenDirectoryDialogComponent {
+
+    repositoryUrl?: string;
+
+    constructor(
+        private electronIpcApiService: ElectronIpcApiService,
+        private popupService: PopupService,
+    ) {
+
+    }
+
+    protected clone = () => this.electronIpcApiService.clone('https://github.com/isomorphic-git/lightning-fs', 'C:/test-repo')
+        .subscribe(() => this.popupService.info('Repository cloned'));
+
+}
