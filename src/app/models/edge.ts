0d6bcfed5e0f2fec94bc99b77ca2f0bedfabfef4
zeuros <gery.zablocki@gmail.com>
2025-03-13 01:58:58 +0100

    feat: improve display, store edtes into interval tree (super cool, thanks gitamine)

---
+++ src/app/models/edge.ts
@@ -0,0 +1,20 @@
+import {RefType} from "../enums/ref-type.enum";
+
+export class Edge {
+
+  public low: number;
+  public high: number;
+
+  constructor(
+    public childRow: number,
+    public childCol: number,
+    public parentRow: number,
+    public parentCol: number,
+    public summary: string,
+    public type: RefType,
+  ) {
+    this.low = childRow;
+    this.high = parentRow;
+  }
+
+}
