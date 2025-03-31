0ea2f65695b9c57e914143130586420db8b85757
zeuros <gery.zablocki@gmail.com>
2025-03-31 23:11:34 +0200

    refactor: bump versions and adapt stuff

--- src/app/utils/repository-utils.ts
+++ src/app/utils/repository-utils.ts
@@ -1,8 +1,5 @@
 import {GitRepository} from "../models/git-repository";
 import {lastFolderName} from "./utils";
-import {TreeNode} from "primeng/api";
-import {Branch} from "../models/branch";
-import {Tree} from "primeng/tree";
 
 export const createRepository = (directory: string) => {
   // TODO: gather all repo infos to create the GitRepository object
