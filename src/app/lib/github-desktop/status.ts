b3c4c9f81eb64d52c0d68f830210899b963c708f
zeuros <13236310+zeuros@users.noreply.github.com>
2025-04-17 01:53:16 +0200

    refactor: cleanup

--- src/app/lib/github-desktop/status.ts
+++ src/app/lib/github-desktop/status.ts
@@ -70,99 +70,99 @@ type ConflictFilesDetails = {
   binaryFilePaths: ReadonlyArray<string>
 }
 
-function parseConflictedState(
-  entry: UnmergedEntry,
-  path: string,
-  conflictDetails: ConflictFilesDetails
-): ConflictedFileStatus {
-  switch (entry.action) {
-    case UnmergedEntrySummary.BothAdded: {
-      const isBinary = conflictDetails.binaryFilePaths.includes(path)
-      if (!isBinary) {
-        return {
-          kind: AppFileStatusKind.Conflicted,
-          entry,
-          conflictMarkerCount:
-            conflictDetails.conflictCountsByPath.get(path) || 0,
-        }
-      } else {
-        return {
-          kind: AppFileStatusKind.Conflicted,
-          entry,
-        }
-      }
-    }
-    case UnmergedEntrySummary.BothModified: {
-      const isBinary = conflictDetails.binaryFilePaths.includes(path)
-      if (!isBinary) {
-        return {
-          kind: AppFileStatusKind.Conflicted,
-          entry,
-          conflictMarkerCount:
-            conflictDetails.conflictCountsByPath.get(path) || 0,
-        }
-      } else {
-        return {
-          kind: AppFileStatusKind.Conflicted,
-          entry,
-        }
-      }
-    }
-    default:
-      return {
-        kind: AppFileStatusKind.Conflicted,
-        entry,
-      }
-  }
-}
+// function parseConflictedState(
+//   entry: UnmergedEntry,
+//   path: string,
+//   conflictDetails: ConflictFilesDetails
+// ): ConflictedFileStatus {
+//   switch (entry.action) {
+//     case UnmergedEntrySummary.BothAdded: {
+//       const isBinary = conflictDetails.binaryFilePaths.includes(path)
+//       if (!isBinary) {
+//         return {
+//           kind: AppFileStatusKind.Conflicted,
+//           entry,
+//           conflictMarkerCount:
+//             conflictDetails.conflictCountsByPath.get(path) || 0,
+//         }
+//       } else {
+//         return {
+//           kind: AppFileStatusKind.Conflicted,
+//           entry,
+//         }
+//       }
+//     }
+//     case UnmergedEntrySummary.BothModified: {
+//       const isBinary = conflictDetails.binaryFilePaths.includes(path)
+//       if (!isBinary) {
+//         return {
+//           kind: AppFileStatusKind.Conflicted,
+//           entry,
+//           conflictMarkerCount:
+//             conflictDetails.conflictCountsByPath.get(path) || 0,
+//         }
+//       } else {
+//         return {
+//           kind: AppFileStatusKind.Conflicted,
+//           entry,
+//         }
+//       }
+//     }
+//     default:
+//       return {
+//         kind: AppFileStatusKind.Conflicted,
+//         entry,
+//       }
+//   }
+// }
 
-function convertToAppStatus(
-  path: string,
-  entry: FileEntry,
-  conflictDetails: ConflictFilesDetails,
-  oldPath?: string
-): AppFileStatus {
-  if (entry.kind === 'ordinary') {
-    switch (entry.type) {
-      case 'added':
-        return {
-          kind: AppFileStatusKind.New,
-          submoduleStatus: entry.submoduleStatus,
-        }
-      case 'modified':
-        return {
-          kind: AppFileStatusKind.Modified,
-          submoduleStatus: entry.submoduleStatus,
-        }
-      case 'deleted':
-        return {
-          kind: AppFileStatusKind.Deleted,
-          submoduleStatus: entry.submoduleStatus,
-        }
-    }
-  } else if (entry.kind === 'copied' && oldPath != null) {
-    return {
-      kind: AppFileStatusKind.Copied,
-      oldPath,
-      submoduleStatus: entry.submoduleStatus,
-    }
-  } else if (entry.kind === 'renamed' && oldPath != null) {
-    return {
-      kind: AppFileStatusKind.Renamed,
-      oldPath,
-      submoduleStatus: entry.submoduleStatus,
-    }
-  } else if (entry.kind === 'untracked') {
-    return {
-      kind: AppFileStatusKind.Untracked,
-      submoduleStatus: entry.submoduleStatus,
-    }
-  } else if (entry.kind === 'conflicted') {
-    return parseConflictedState(entry, path, conflictDetails)
-  }
-
-  return throwEx(`Unknown file status ${status}`)
-}
+// function convertToAppStatus(
+//   path: string,
+//   entry: FileEntry,
+//   conflictDetails: ConflictFilesDetails,
+//   oldPath?: string
+// ): AppFileStatus {
+//   if (entry.kind === 'ordinary') {
+//     switch (entry.type) {
+//       case 'added':
+//         return {
+//           kind: AppFileStatusKind.New,
+//           submoduleStatus: entry.submoduleStatus,
+//         }
+//       case 'modified':
+//         return {
+//           kind: AppFileStatusKind.Modified,
+//           submoduleStatus: entry.submoduleStatus,
+//         }
+//       case 'deleted':
+//         return {
+//           kind: AppFileStatusKind.Deleted,
+//           submoduleStatus: entry.submoduleStatus,
+//         }
+//     }
+//   } else if (entry.kind === 'copied' && oldPath != null) {
+//     return {
+//       kind: AppFileStatusKind.Copied,
+//       oldPath,
+//       submoduleStatus: entry.submoduleStatus,
+//     }
+//   } else if (entry.kind === 'renamed' && oldPath != null) {
+//     return {
+//       kind: AppFileStatusKind.Renamed,
+//       oldPath,
+//       submoduleStatus: entry.submoduleStatus,
+//     }
+//   } else if (entry.kind === 'untracked') {
+//     return {
+//       kind: AppFileStatusKind.Untracked,
+//       submoduleStatus: entry.submoduleStatus,
+//     }
+//   } else if (entry.kind === 'conflicted') {
+//     return parseConflictedState(entry, path, conflictDetails)
+//   }
+//
+//   return throwEx(`Unknown file status ${status}`)
+// }
 
 // List of known conflicted index entries for a file, extracted from mapStatus
 // inside `app/src/lib/status-parser.ts` for convenience
@@ -175,56 +175,56 @@ const conflictStatusCodes = ['DD', 'AU', 'UD', 'UA', 'DU', 'AA', 'UU']
  *
  * (Map is used here to maintain insertion order.)
  */
-function buildStatusMap(
-  files: Map<string, WorkingDirectoryFileChange>,
-  entry: IStatusEntry,
-  conflictDetails: ConflictFilesDetails
-): Map<string, WorkingDirectoryFileChange> {
-  const status = mapStatus(entry.statusCode, entry.submoduleStatusCode)
-
-  if (status.kind === 'ordinary') {
-    // when a file is added in the index but then removed in the working
-    // directory, the file won't be part of the commit, so we can skip
-    // displaying this entry in the changes list
-    if (
-      status.index === GitStatusEntry.Added &&
-      status.workingTree === GitStatusEntry.Deleted
-    ) {
-      return files
-    }
-  }
-
-  if (status.kind === 'untracked') {
-    // when a delete has been staged, but an untracked file exists with the
-    // same path, we should ensure that we only draw one entry in the
-    // changes list - see if an entry already exists for this path and
-    // remove it if found
-    files.delete(entry.path)
-  }
-
-  // for now we just poke at the existing summary
-  const appStatus = convertToAppStatus(
-    entry.path,
-    status,
-    conflictDetails,
-    entry.oldPath
-  )
-
-  const initialSelectionType =
-    appStatus.kind === AppFileStatusKind.Modified &&
-    appStatus.submoduleStatus !== undefined &&
-    !appStatus.submoduleStatus.commitChanged
-      ? DiffSelectionType.None
-      : DiffSelectionType.All
-
-  const selection = DiffSelection.fromInitialSelection(initialSelectionType)
-
-  files.set(
-    entry.path,
-    new WorkingDirectoryFileChange(entry.path, appStatus, selection)
-  )
-  return files
-}
+// function buildStatusMap(
+//   files: Map<string, WorkingDirectoryFileChange>,
+//   entry: IStatusEntry,
+//   conflictDetails: ConflictFilesDetails
+// ): Map<string, WorkingDirectoryFileChange> {
+//   const status = mapStatus(entry.statusCode, entry.submoduleStatusCode)
+//
+//   if (status.kind === 'ordinary') {
+//     // when a file is added in the index but then removed in the working
+//     // directory, the file won't be part of the commit, so we can skip
+//     // displaying this entry in the changes list
+//     if (
+//       status.index === GitStatusEntry.Added &&
+//       status.workingTree === GitStatusEntry.Deleted
+//     ) {
+//       return files
+//     }
+//   }
+//
+//   if (status.kind === 'untracked') {
+//     // when a delete has been staged, but an untracked file exists with the
+//     // same path, we should ensure that we only draw one entry in the
+//     // changes list - see if an entry already exists for this path and
+//     // remove it if found
+//     files.delete(entry.path)
+//   }
+//
+//   // for now we just poke at the existing summary
+//   const appStatus = convertToAppStatus(
+//     entry.path,
+//     status,
+//     conflictDetails,
+//     entry.oldPath
+//   )
+//
+//   const initialSelectionType =
+//     appStatus.kind === AppFileStatusKind.Modified &&
+//     appStatus.submoduleStatus !== undefined &&
+//     !appStatus.submoduleStatus.commitChanged
+//       ? DiffSelectionType.None
+//       : DiffSelectionType.All
+//
+//   const selection = DiffSelection.fromInitialSelection(initialSelectionType)
+//
+//   files.set(
+//     entry.path,
+//     new WorkingDirectoryFileChange(entry.path, appStatus, selection)
+//   )
+//   return files
+// }
 
 /**
  * Update status header based on the current header entry.
@@ -293,7 +293,7 @@ const getWorkingDirectoryConflictDetails =
       .pipe(catchError((e, caught) => {
         console.warn(e); // its totally fine if HEAD doesn't exist, which throws an error
         return caught
-    }))
+      }))
 
 /**
  * gets the conflicted files count and binary file paths in a given repository.
