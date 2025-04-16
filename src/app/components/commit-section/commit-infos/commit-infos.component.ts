1f0ee0d10f4e1b668af17fc17cb48211a38fd252
zeuros <13236310+zeuros@users.noreply.github.com>
2025-04-17 01:08:09 +0200

    feat: more code for commit infos sections

--- src/app/components/commit-section/commit-infos/commit-infos.component.ts
+++ src/app/components/commit-section/commit-infos/commit-infos.component.ts
@@ -1,11 +1,25 @@
 import {Component, effect, inject, input} from '@angular/core';
-import {NgIf} from "@angular/common";
 import {CommitFilesChangesService} from "../../../services/electron-cmd-parser-layer/commit-files-changes.service";
+import {DisplayRef} from "../../../lib/github-desktop/model/display-ref";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {InputText} from "primeng/inputtext";
+import {Textarea} from "primeng/textarea";
+import {ButtonDirective} from "primeng/button";
+import {JsonPipe, NgForOf} from "@angular/common";
+import {isEqual} from "lodash";
+import {Avatar} from "primeng/avatar";
+import {hasName, initials} from "../../../utils/commit-utils";
 
 @Component({
   selector: 'gitgud-commit-infos',
   imports: [
-    NgIf
+    ReactiveFormsModule,
+    InputText,
+    Textarea,
+    ButtonDirective,
+    JsonPipe,
+    NgForOf,
+    Avatar
   ],
   templateUrl: './commit-infos.component.html',
   styleUrl: './commit-infos.component.scss'
@@ -14,13 +28,31 @@ export class CommitInfosComponent {
 
   private commitFilesChangesService = inject(CommitFilesChangesService);
 
-  selectedCommits = input<string[]>([]);
+  selectedCommits = input<DisplayRef[]>([]);
+
+  protected editCommitForm = new FormGroup({
+    summary: new FormControl(''),
+    description: new FormControl(''),
+  });
+  protected initialValue: typeof this.editCommitForm.value = {};
+
 
   constructor() {
     effect(() => {
-      if (this.selectedCommits().length)
-        this.commitFilesChangesService.getChangedFilesForGivenCommit(this.selectedCommits()[0]).subscribe(console.log);
-    })
+      if (this.selectedCommits().length == 1) {
+        this.commitFilesChangesService.getChangedFilesForGivenCommit(this.selectedCommits()[0].sha).subscribe(console.log);
+        this.editCommitForm.setValue({
+          summary: this.selectedCommits()[0].summary,
+          description: this.selectedCommits()[0].body
+        });
+        this.initialValue = this.editCommitForm.value;
+
+      } else if (this.selectedCommits().length > 1)
+        console.warn('TODO');
+    });
   }
 
+  protected readonly isEqual = isEqual;
+  protected readonly initials = initials;
+  protected readonly hasName = hasName;
 }
--- src/app/components/commit-section/commit-section.component.ts
+++ src/app/components/commit-section/commit-section.component.ts
@@ -1,12 +1,9 @@
-import {Component, inject, input} from '@angular/core';
-import {toObservable} from "@angular/core/rxjs-interop";
-import {notUndefined} from "../../utils/utils";
-import {filter, switchMap} from "rxjs";
-import {CommitFilesChangesService} from "../../services/electron-cmd-parser-layer/commit-files-changes.service";
+import {Component, input} from '@angular/core';
 import {ReactiveFormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
 import {CommitInfosComponent} from "./commit-infos/commit-infos.component";
 import {MakeACommitComponent} from "./make-a-commit/make-a-commit.component";
+import {DisplayRef} from "../../lib/github-desktop/model/display-ref";
 
 @Component({
   selector: 'gitgud-commit-section',
@@ -22,6 +19,6 @@ import {MakeACommitComponent} from "./make-a-commit/make-a-commit.component";
 })
 export class CommitSectionComponent {
 
-  selectedCommits = input<string[]>();
+  selectedCommits = input<DisplayRef[]>([]);
 
 }
