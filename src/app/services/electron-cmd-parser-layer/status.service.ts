72398ef68cbf6057b2ea1ba19ef8360e6be2c3f8
zeuros <gery.zablocki@gmail.com>
2025-04-17 01:53:16 +0200

    refactor: cleanup

--- src/app/services/electron-cmd-parser-layer/status.service.ts
+++ src/app/services/electron-cmd-parser-layer/status.service.ts
@@ -17,69 +17,69 @@ export class StatusService {
    *  Retrieve the status for a given repository,
    *  and fail gracefully if the location is not a Git repository
    */
-  getStatus = () =>
-    this.gitRepositoryService.git([
-      '--no-optional-locks',
-      'status',
-      '--untracked-files=all',
-      '--branch',
-      '--porcelain=2',
-      '-z',
-    ]).pipe(map(statusRaw => {
-      const parsed = parsePorcelainStatus(new Buffer(statusRaw))
-      const headers = parsed.filter(isStatusHeader)
-      const entries = parsed.filter(isStatusEntry)
-
-      const mergeHeadFound = isMergeHeadSet()
-      const conflictedFilesInIndex = entries.filter(e => conflictStatusCodes.includes(e.statusCode))
-      // const rebaseInternalState = await getRebaseInternalState(repository)
-
-      const conflictDetails = await getConflictDetails(
-        repository,
-        mergeHeadFound,
-        conflictedFilesInIndex,
-        rebaseInternalState
-      )
-
-      // Map of files keyed on their paths.
-      const files = entries.reduce(
-        (files, entry) => buildStatusMap(files, entry, conflictDetails),
-        new Map<string, WorkingDirectoryFileChange>()
-      )
-
-      const {
-        currentBranch,
-        currentUpstreamBranch,
-        currentTip,
-        branchAheadBehind,
-      } = headers.reduce(parseStatusHeader, {
-        currentBranch: undefined,
-        currentUpstreamBranch: undefined,
-        currentTip: undefined,
-        branchAheadBehind: undefined,
-        match: null,
-      })
-
-      const workingDirectory = WorkingDirectoryStatus.fromFiles([...files.values()])
-
-      const isCherryPickingHeadFound = await isCherryPickHeadFound(repository)
-
-      const squashMsgFound = await isSquashMsgSet(repository)
-
-      return {
-        currentBranch,
-        currentTip,
-        currentUpstreamBranch,
-        branchAheadBehind,
-        exists: true,
-        mergeHeadFound,
-        rebaseInternalState,
-        workingDirectory,
-        isCherryPickingHeadFound,
-        squashMsgFound,
-        doConflictedFilesExist: conflictedFilesInIndex.length > 0,
-      }
-    }));
+  // getStatus = () =>
+  //   this.gitRepositoryService.git([
+  //     '--no-optional-locks',
+  //     'status',
+  //     '--untracked-files=all',
+  //     '--branch',
+  //     '--porcelain=2',
+  //     '-z',
+  //   ]).pipe(map(statusRaw => {
+  //     const parsed = parsePorcelainStatus(new Buffer(statusRaw))
+  //     const headers = parsed.filter(isStatusHeader)
+  //     const entries = parsed.filter(isStatusEntry)
+  //
+  //     const mergeHeadFound = isMergeHeadSet()
+  //     const conflictedFilesInIndex = entries.filter(e => conflictStatusCodes.includes(e.statusCode))
+  //     // const rebaseInternalState = await getRebaseInternalState(repository)
+  //
+  //     const conflictDetails = getConflictDetails(
+  //       this.gitRepositoryService.git,
+  //       mergeHeadFound,
+  //       conflictedFilesInIndex,
+  //       rebaseInternalState
+  //     )
+  //
+  //     // Map of files keyed on their paths.
+  //     const files = entries.reduce(
+  //       (files, entry) => buildStatusMap(files, entry, conflictDetails),
+  //       new Map<string, WorkingDirectoryFileChange>()
+  //     )
+  //
+  //     const {
+  //       currentBranch,
+  //       currentUpstreamBranch,
+  //       currentTip,
+  //       branchAheadBehind,
+  //     } = headers.reduce(parseStatusHeader, {
+  //       currentBranch: undefined,
+  //       currentUpstreamBranch: undefined,
+  //       currentTip: undefined,
+  //       branchAheadBehind: undefined,
+  //       match: null,
+  //     })
+  //
+  //     const workingDirectory = WorkingDirectoryStatus.fromFiles([...files.values()])
+  //
+  //     const isCherryPickingHeadFound = await isCherryPickHeadFound(repository)
+  //
+  //     const squashMsgFound = await isSquashMsgSet(repository)
+  //
+  //     return {
+  //       currentBranch,
+  //       currentTip,
+  //       currentUpstreamBranch,
+  //       branchAheadBehind,
+  //       exists: true,
+  //       mergeHeadFound,
+  //       rebaseInternalState,
+  //       workingDirectory,
+  //       isCherryPickingHeadFound,
+  //       squashMsgFound,
+  //       doConflictedFilesExist: conflictedFilesInIndex.length > 0,
+  //     }
+  //   }));
 
 }
 
