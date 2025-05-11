62e0d04bdb09014144d8c6a905039caedd80113b
zeuros <gery.zablocki@gmail.com>
2025-05-12 01:45:47 +0200

    feat: nice

--- src/app/components/commit-section/commit-infos/avatar/avatar.component.ts
+++ src/app/components/commit-section/commit-infos/avatar/avatar.component.ts
@@ -1,19 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {CommitIdentity} from "../../../../lib/github-desktop/model/commit-identity";
 import {GravatarUrlPipe} from "../../../../pipes/gravatar-url";
-import {NgIf, NgOptimizedImage} from "@angular/common";
+import {NgIf} from "@angular/common";
 import {IdenticonPipe} from "../../../../services/identicon-pipe.service";
+import {fadeSlideIn} from "../../../../shared/animations";
 
 @Component({
   selector: 'gitgud-avatar',
   imports: [
     GravatarUrlPipe,
     NgIf,
-    NgOptimizedImage,
     IdenticonPipe
   ],
   templateUrl: './avatar.component.html',
-  styleUrl: './avatar.component.scss'
+  styleUrl: './avatar.component.scss',
+  animations: [fadeSlideIn]
 })
 export class AvatarComponent {
 
