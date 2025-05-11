6a876619920635b322e3a55272e321dae21d7f87
zeuros <13236310+zeuros@users.noreply.github.com>
2025-05-12 01:45:47 +0200

    feat: nice

---
+++ src/app/shared/animations.ts
@@ -0,0 +1,21 @@
+import {animate, style, transition, trigger} from '@angular/animations';
+
+export const fadeSlideInOut =
+  trigger('fadeSlideInOut', [
+    transition(':enter', [
+      style({opacity: 0, transform: 'translateY(10px)'}),
+      animate('500ms', style({opacity: 1, transform: 'translateY(0)'})),
+    ]),
+    transition(':leave', [
+      animate('500ms', style({opacity: 0, transform: 'translateY(10px)'})),
+    ]),
+  ]);
+
+
+export const fadeSlideIn =
+  trigger('fadeSlideIn', [
+    transition(':enter', [
+      style({opacity: 0, transform: 'translateY(6px)'}),
+      animate('150ms', style({opacity: 1, transform: 'translateY(0)'})),
+    ]),
+  ]);
