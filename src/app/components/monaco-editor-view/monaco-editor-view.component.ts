8af0f83461b1e34c75708a6e430e5bfa3775cca7
zeuros <13236310+zeuros@users.noreply.github.com>
2025-05-27 01:53:40 +0200

    feat: Monaco editor works !

--- src/app/components/monaco-editor-view/monaco-editor-view.component.ts
+++ src/app/components/monaco-editor-view/monaco-editor-view.component.ts
@@ -1,12 +1,25 @@
 import {Component} from '@angular/core';
+import {DiffEditorComponent, DiffEditorModel} from 'ngx-monaco-editor-v2';
 
 @Component({
   standalone: true,
   selector: 'gitgud-monaco-editor-view',
-  imports: [],
+  imports: [DiffEditorComponent],
   templateUrl: './monaco-editor-view.component.html',
   styleUrl: './monaco-editor-view.component.scss',
 })
 export class MonacoEditorViewComponent {
+  options = {
+    theme: 'vs-dark',
+  };
+  originalModel: DiffEditorModel = {
+    code: 'heLLo world!',
+    language: 'text/plain',
+  };
+
+  modifiedModel: DiffEditorModel = {
+    code: 'hello orlando!',
+    language: 'text/plain',
+  };
 
 }
