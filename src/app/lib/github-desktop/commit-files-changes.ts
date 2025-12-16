9a8d6caf528583fdf697c4f35b1df0a59927d1dd
zeuros <gery.zablocki@gmail.com>
2025-12-16 03:26:22 +0100

    feat: show modified files in staging area

--- src/app/lib/github-desktop/commit-files-changes.ts
+++ src/app/lib/github-desktop/commit-files-changes.ts
@@ -27,63 +27,63 @@
  *    file_two_new_path
  */
 
-import {CommittedFileChange} from "./model/change-set";
-import {forceUnwrap} from "./throw-ex";
-import {isCopyOrRename, mapStatus} from "./log";
-import {getNoRenameIndexStatus, NoRenameIndexStatus} from "./model/diff-index";
-import {AppFileStatus} from "./model/status";
+import {CommittedFileChange} from './model/change-set';
+import {forceUnwrap} from './throw-ex';
+import {isCopyOrRename, mapStatus} from './log';
+import {IndexStatus} from './model/diff-index';
+import {AppFileStatus} from './model/status';
 
 export const parseRawLogWithNumstat = (rawFileChanges: string, sha: string) => {
-  const files = new Array<CommittedFileChange>()
-  let linesAdded = 0
-  let linesDeleted = 0
-  let numStatCount = 0
+  const files = new Array<CommittedFileChange>();
+  let linesAdded = 0;
+  let linesDeleted = 0;
+  let numStatCount = 0;
   const lines = rawFileChanges.split('\0');
 
-  for (let i = 0; i < lines.length - 1; i++) {
-    const line = lines[i]
+  for (let i = 0 ; i < lines.length - 1 ; i++) {
+    const line = lines[i];
     if (line.startsWith(':')) {
       const lineComponents = line.split(' ');
       const status = forceUnwrap('Invalid log output (status)', lineComponents.at(-1));
       const oldPath = /^R|C/.test(status)
         ? forceUnwrap('Missing old path', lines.at(++i))
-        : undefined
+        : undefined;
 
       const path = forceUnwrap('Missing path', lines.at(++i));
 
       files.push(new CommittedFileChange(path, {kind: mapStatus(status, oldPath), oldPath} as AppFileStatus, sha));
     } else {
-      const match = /^(\d+|-)\t(\d+|-)\t/.exec(line)
-      const [, added, deleted] = forceUnwrap('Invalid numstat line', match)
-      linesAdded += added === '-' ? 0 : parseInt(added, 10)
-      linesDeleted += deleted === '-' ? 0 : parseInt(deleted, 10)
+      const match = /^(\d+|-)\t(\d+|-)\t/.exec(line);
+      const [, added, deleted] = forceUnwrap('Invalid numstat line', match);
+      linesAdded += added === '-' ? 0 : parseInt(added, 10);
+      linesDeleted += deleted === '-' ? 0 : parseInt(deleted, 10);
 
       // If this entry denotes a rename or copy the old and new paths are on
       // two separate fields (separated by \0). Otherwise, they're on the same
       // line as the added and deleted lines.
       if (isCopyOrRename(files[numStatCount].status)) {
-        i += 2
+        i += 2;
       }
-      numStatCount++
+      numStatCount++;
     }
   }
 
-  return {files, linesAdded, linesDeleted}
-}
+  return {files, linesAdded, linesDeleted};
+};
 
-export const parseIndexChanges = (indexChangesRawResult: string) => {
+type FilesAndStatuses = { [key: string]: IndexStatus };
 
-  const map = new Map<string, NoRenameIndexStatus>()
+export const parseIndexChanges = (indexChangesRawResult: string): FilesAndStatuses => {
 
-  const pieces = indexChangesRawResult.split('\0')
+  const map: FilesAndStatuses = {};
 
-  for (let i = 0; i < pieces.length - 1; i += 2) {
-    const status = getNoRenameIndexStatus(pieces[i])
-    const path = pieces[i + 1]
+  const pieces = indexChangesRawResult.split('\0');
 
-    if (status)
-      map.set(path, status)
+  for (let i = 0 ; i < pieces.length - 1 ; i += 2) {
+    const path = pieces[i + 1];
+
+    if (pieces[i]) map[path] = pieces[i] as IndexStatus;
   }
 
-  return map
-}
+  return map;
+};
--- src/app/lib/github-desktop/model/diff-index.ts
+++ src/app/lib/github-desktop/model/diff-index.ts
@@ -1,5 +1,3 @@
-import {throwEx} from "../../../utils/utils";
-
 /**
  * Possible statuses of an entry in Git, see the git diff-index
  * man page for additional details.
@@ -28,23 +26,6 @@ export type NoRenameIndexStatus =
   | IndexStatus.TypeChanged
   | IndexStatus.Unmerged
   | IndexStatus.Unknown
-
-const getIndexStatus = (status: string): IndexStatus | never =>
-  Object.values(IndexStatus).includes(status as IndexStatus)
-    ? status as IndexStatus
-    : throwEx(`Wrong IndexStatus: ${status}`);
-
-export function getNoRenameIndexStatus(status: string): NoRenameIndexStatus | undefined {
-  const parsed = getIndexStatus(status)
-
-  switch (parsed) {
-    case IndexStatus.Copied:
-    case IndexStatus.Renamed:
-      throw new Error(`Invalid index status for no-rename index status: ${parsed}`)
-  }
-
-  return parsed
-}
 
 /** The SHA for the null tree. */
 export const NullTreeSHA = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
