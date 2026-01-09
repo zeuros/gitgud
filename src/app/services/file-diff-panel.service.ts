1ba35c6edc18d2336a44303c4425313627afbb58
zeuros <13236310+zeuros@users.noreply.github.com>
2026-01-09 18:08:28 +0100

    feat: update monaco editor view on live file update

--- src/app/services/file-diff-panel.service.ts
+++ src/app/services/file-diff-panel.service.ts
@@ -1,15 +1,37 @@
-import {Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {CommittedFileChange, FileChange, WorkingDirectoryFileChange} from '../lib/github-desktop/model/status';
-import {Subject} from 'rxjs';
+import {EMPTY, map, of, Subject, switchMap} from 'rxjs';
+import {CommitFilesChangesService} from './electron-cmd-parser-layer/commit-files-changes.service';
+import {instanceOf} from '../utils/utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileDiffPanelService {
 
-  showCommittedFileDiffs = (f: CommittedFileChange) => this.fileToDiff$.next(f);
-  showWorkingDirDiffs = (f: WorkingDirectoryFileChange) => this.fileToDiff$.next(f);
+  private readonly commitFilesChangesService = inject(CommitFilesChangesService);
+  private fileToDiffSubject$ = new Subject<FileChange | null>();
 
-  fileToDiff$ = new Subject<FileChange>();
+  // Here we update monaco view for commit files (no live changes) or for working dir changes (live update on file change)
+  fileToDiff$ = this.fileToDiffSubject$.pipe(
+    switchMap(file => {
+
+      // Explicit null → emit null
+      if (!file) return of(file);
+
+      // Committed file → emit once, file won't change
+      if (instanceOf(file, CommittedFileChange)) return of(file);
+
+      // Working directory file → re-emit on file diff update
+      if (instanceOf(file, WorkingDirectoryFileChange))
+        return this.commitFilesChangesService.workingDirChanges$.pipe(map(() => ({...file}))); // force new reference to update editor on file change
+
+      return EMPTY;
+    }),
+  );
+
+  showCommittedFileDiffs = (f: CommittedFileChange) => this.fileToDiffSubject$.next(f);
+
+  showWorkingDirDiffs = (f: WorkingDirectoryFileChange) => this.fileToDiffSubject$.next(f);
 
 }
