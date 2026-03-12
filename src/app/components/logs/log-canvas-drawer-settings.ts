32369d2eec44658ad6315fbba933203f68209c0e
zeuros <13236310+zeuros@users.noreply.github.com>
2026-03-12 01:19:46 +0100

    refactor: move canvas drawing code to dedicated file

---
+++ src/app/components/logs/log-canvas-drawer-settings.ts
@@ -0,0 +1,6 @@
+export const CANVAS_MARGIN = [5, 1];
+export const NODE_DIAMETER = 26;
+export const NODES_VERTICAL_SPACING = 8;
+export const COMMITS_SHOWN_ON_CANVAS = 37; // TODO: change it on screen resize depending table row count
+export const NODE_RADIUS = NODE_DIAMETER / 2;
+export const ROW_HEIGHT = NODE_DIAMETER + NODES_VERTICAL_SPACING;
