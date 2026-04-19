7a5047c0ad4557b27e8c324450e28585d2e724a1
zeuros <13236310+zeuros@users.noreply.github.com>
2026-04-19 19:39:41 +0200

    chore: license

--- src/app/services/electron-cmd-parser-layer/git-api.service.ts
+++ src/app/services/electron-cmd-parser-layer/git-api.service.ts
@@ -1,3 +1,21 @@
+/*
+ * GitGud - A Git GUI client
+ * Copyright (C) 2026 zeuros
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import {Injectable, isDevMode, signal} from '@angular/core';
 
 // If you import a module but never use any of the imported values other than as TypeScript types,
